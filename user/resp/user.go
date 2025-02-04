55bc3d8a02851a888d526008bc818f6f88cda7f5
huhx <gohuhx@163.com>
2025-02-04 20:49:03 +0800

    [huhx] -feat: correct the type for id in user

--- user/resp/user.go
+++ user/resp/user.go
@@ -7,7 +7,7 @@ type TokenResponse struct {
 }
 
 type UserListRespItem struct {
-	Id       string `json:"id"`
+	Id       int64  `json:"id"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	Nickname string `json:"nickname"`
@@ -15,10 +15,9 @@ type UserListRespItem struct {
 }
 
 type UserDetailResp struct {
-	Id          string          `json:"id"`
+	Id          int64           `json:"id"`
 	Name        string          `json:"name"`
 	Username    string          `json:"username"`
-	Password    string          `json:"password"`
 	Nickname    string          `json:"nickname"`
 	Avatar      string          `json:"avatar"`
 	Birthday    times.LocalDate `json:"birthday"`
