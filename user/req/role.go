eded9a8510e50b42ef7b505880bd694ef857b2ea
huhx <gohuhx@163.com>
2025-02-05 07:16:20 +0800

    [huhx] -feat: add RoleAddMenusRequest

--- user/req/role.go
+++ user/req/role.go
@@ -10,3 +10,7 @@ type RoleUpdateRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+type RoleAddMenusRequest struct {
+	MenuIds []int64 `json:"menuIds"`
+}
