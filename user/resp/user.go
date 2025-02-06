18c30de4b66e54569ad6475b8db00143691dd63d
huhx <gohuhx@163.com>
2025-02-06 18:57:09 +0800

    [huhx] -feat: add pure user resp

--- user/resp/user.go
+++ user/resp/user.go
@@ -29,3 +29,19 @@ type UserDetailResp struct {
 	RoleId      int64           `json:"roleId"` // boss, emp, admin
 	FailedCount int             `json:"failedCount"`
 }
+
+type PureUserResp struct {
+	Id          int64           `json:"id"`
+	Name        string          `json:"name"`
+	Username    string          `json:"username"`
+	Nickname    string          `json:"nickname"`
+	Avatar      string          `json:"avatar"`
+	Birthday    times.LocalDate `json:"birthday"`
+	Address     string          `json:"address"`
+	Signature   string          `json:"signature"`
+	Email       string          `json:"email"`
+	Phone       string          `json:"phone"`
+	Wechat      string          `json:"wechat"`
+	RoleId      int64           `json:"roleId"` // boss, emp, admin
+	FailedCount int             `json:"failedCount"`
+}
